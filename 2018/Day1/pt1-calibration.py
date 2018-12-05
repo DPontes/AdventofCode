94295453bec189783bdc9bee33acda44bbf130a5
DPontes <dpontes@protonmail.com>
2018-12-05 23:41:05 +0100

    Simplification, as 'int(line)' is sufficient
    	'int(line)' returns the true value of the string, no need to check the signal

--- 2018/Day1/pt1-calibration.py
+++ 2018/Day1/pt1-calibration.py
@@ -1,25 +1,12 @@
 #!/usr/bin/python
 
 
-def lineValue(line):
-    # Returns the value of the line as an int
-    return int(line[1:])
-
-
-def sumOrSub(line):
-    # Returns the first char in the line as +1 or -1
-    if line[0] == '+':
-        return 1
-    else:
-        return -1
-
-
 def getTheTotal():
     # Returns the sum of the input
     f = open('input.txt')
     sum = 0
     for line in f:
-        sum += sumOrSub(line) * lineValue(line)
+        sum += int(line)
     return sum
 
 
