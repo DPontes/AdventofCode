e651a5e0f3e31c233b2e2da4431d60c21ec10ba6
DPontes <dpontes@protonmail.com>
2018-12-11 11:56:22 +0100

    Pycodestyle convention

--- 2018/Day2/pt1-checksum.py
+++ 2018/Day2/pt1-checksum.py
@@ -4,16 +4,17 @@ from collections import Counter
 
 def getLineOccurences(line):
     total = Counter(list(Counter(line).values()))
-    two = 0 if total.get(2) == None else 1
-    three = 0 if total.get(3) == None else 1
+    # these two lines below are very ugly, should be refactored
+    two = 0 if total.get(2) is None else 1
+    three = 0 if total.get(3) is None else 1
     return two, three
+
 
 def getCheckSum():
     f = open('input.txt')
     check = [0, 0]
     for line in f:
         two, three = getLineOccurences(line)
-        print(two, three)
         check[0] += two
         check[1] += three
     return check[0] * check[1]
