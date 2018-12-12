ddcd66f3ba5252de76e3afbdde584a7379280b9b
DPontes <dpontes@protonmail.com>
2018-12-12 15:10:15 +0100

    Getting closer, but current solution doesn't work
    	Current solution doesn't work when unique chars that it finds
    	are present somewhere else in one of the strings. F.ex.:
    		'auxwcbzrmdvpsjfgkrthnkioqm'
    	and
    		'auxwcbgrmdvpsjfgkdthnrioqe'
    	differ in:
    		zrkm
    		gdre
    	but because 'rkm' appear somewhere else in the string, these
    	differences are not taken into consideration, and therefore
    	function 'getUniqueChars(...)' returns incorrect output

--- 2018/Day2/pt2-commonLetters.py
+++ 2018/Day2/pt2-commonLetters.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python
 
+def removeUniqueChars(unique, str1, str2):
+    for letter in unique:
+        if letter in str1:
+            return str1.replace(letter, '')
+
 def getUniqueChars(str1, str2):
     # Returns a list of unique chars of the input strings
+    # TODO (diogo): This technique doesn work when the differenting
+    # char appears somewhere else in the string
     return list(set(str1)^set(str2))
 
 def loopThrough():
-    str1 = open('example-input.txt').readline()
-    print(str1)
+    f = open('input.txt').readlines()
+    for str1 in f:
+        for str2 in f:
+            unique = getUniqueChars(str1, str2)
+            if len(unique) < 3 and len(unique) > 0:
+                print(str1,str2)
+                return removeUniqueChars(unique, str1, str2)
+    return "Done"
 
 if __name__ == '__main__':
-    loopThrough()
+    print(loopThrough())
