9558004f7c6f934afbca448c9e2e94840d08b269
DPontes <dpontes@protonmail.com>
2018-12-14 13:59:42 +0100

    Small correction

--- 2018/Day3/pt1-overlap.py
+++ 2018/Day3/pt1-overlap.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python
 
-f = open('example-input.txt').readlines()
 
 def extractCoordinates(line):
     coord = line.split(' ')[2].split(',')
     coord[1] = coord[1][:-1]  # Removes the ':' at the end
     return coord
 
+
 def extractDimensions(line):
     return line.split(' ')[3].split('x')
+
 
 def defineArea(coords, dimens, rectArea):
     pass
 
+
 if __name__ == '__main__':
+    f = open('example-input.txt').readlines()
     rectArea = []
     for line in f:
         coords = extractCoordinates(line)
