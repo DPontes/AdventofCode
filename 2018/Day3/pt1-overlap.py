634dfcb45bb640ae3e7a867a9245b08939ab7e7c
DPontes <dpontes@protonmail.com>
2018-12-14 13:55:41 +0100

    [WIP] Has some helper functions
    	Helper functions transform the information in each claim to:
    	- Claim start coordinate
    	- Claim dimensions
    
    	Function 'defineArea()' needs a loop to fill up the rectArea[]
    	with used up sq. inches

--- 2018/Day3/pt1-overlap.py
+++ 2018/Day3/pt1-overlap.py
@@ -1,3 +1,21 @@
 #!/usr/bin/python
 
+f = open('example-input.txt').readlines()
 
+def extractCoordinates(line):
+    coord = line.split(' ')[2].split(',')
+    coord[1] = coord[1][:-1]  # Removes the ':' at the end
+    return coord
+
+def extractDimensions(line):
+    return line.split(' ')[3].split('x')
+
+def defineArea(coords, dimens, rectArea):
+    pass
+
+if __name__ == '__main__':
+    rectArea = []
+    for line in f:
+        coords = extractCoordinates(line)
+        dimens = extractDimensions(line)
+        defineArea(coords, dimens, rectArea)
