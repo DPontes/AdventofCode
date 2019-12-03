9d62c6024cd7510ecc3aeed668519193983f6051
DPontes <dpontes@protonmail.com>
2019-12-03 23:15:36 +0100

    Pt2 completed, changed Pt1 file name

---
+++ 2019/Day1/Pt2-moreFuelCalc.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import math
+
+
+def calculateTotalFuel(inputFuel):
+    fuel = 0
+    fuel += math.floor(inputFuel / 3) - 2
+    if fuel > 0:
+        fuel += calculateTotalFuel(fuel)
+        return fuel
+    return 0
+
+if __name__ == '__main__':
+    f = open('input.txt')
+    totalFuel = 0
+    for line in f:
+        totalFuel += calculateTotalFuel(int(line))
+    print(totalFuel)
