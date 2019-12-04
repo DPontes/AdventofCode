9ba3d00decd78f50dec91fe040b276448cfc137a
DPontes <dpontes@protonmail.com>
2019-12-04 13:08:54 +0100

    Improved pytest

--- 2019/Day1/Pt1fuelCalc.py
+++ 2019/Day1/Pt1fuelCalc.py
@@ -2,13 +2,13 @@
 import math
 
 
-def calculateMass():
-    f = open('input.txt')
-    sum = 0
-    for line in f:
-        sum += math.floor(int(line) / 3) - 2
-    return sum
+def calculateFuel(fuel):
+        return math.floor(int(fuel) / 3) - 2
 
 
 if __name__ == '__main__':
-    print(calculateMass())
+    f = open('input.txt')
+    sum = 0
+    for line in f:
+        sum += calculateFuel(line)
+    print(sum)
--- 2019/Day1/test_Pt1-fuelCalc.py
+++ 2019/Day1/test_Pt1-fuelCalc.py
@@ -1,5 +1,8 @@
 import pytest
 from Pt1fuelCalc import *
 
-def test_calculateMass():
-    assert calculateMass() == 3464458
+def test_calculateFuel():
+    assert calculateFuel(12) == 2
+    assert calculateFuel(14) == 2
+    assert calculateFuel(1969) == 654
+    assert calculateFuel(100756) == 33583
