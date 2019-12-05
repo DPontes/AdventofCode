d4f11e3802a09399c32341f7cd153951b212cced
DPontes <dpontes@protonmail.com>
2019-12-05 11:00:34 +0100

    Added CSV input processing, with corresponding passing test

--- 2019/Day2/Pt1_1202Alarm.py
+++ 2019/Day2/Pt1_1202Alarm.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+import csv
 
 
-def intCode(input):
+def readValues():
+    with open('input.txt', 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        value_list = list(reader)
+    return value_list[0]
+
+
+def intCode(value_list):
     return
