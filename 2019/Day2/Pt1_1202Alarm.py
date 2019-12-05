34ed808e9d449a3aa506a3864a40535a5dcaf599
DPontes <dpontes@protonmail.com>
2019-12-05 16:07:56 +0100

    Gives correct answer, has test coverage

--- 2019/Day2/Pt1_1202Alarm.py
+++ 2019/Day2/Pt1_1202Alarm.py
@@ -10,4 +10,28 @@ def readValues():
 
 
 def intCode(value_list):
-    return
+    for element in range(0, len(value_list), 4):
+
+        value_pos0 = int(value_list[int(element)])
+        if value_pos0 == 99: break
+
+        value_pos1 = int(value_list[int(element) + 1])
+        value_pos2 = int(value_list[int(element) + 2])
+        value_pos3 = int(value_list[int(element) + 3])
+
+        if value_pos0 == 1:
+            result = int(value_list[value_pos1]) + int(value_list[value_pos2])
+            value_list[value_pos3] = str(result)
+
+        elif value_pos0 == 2:
+            result = int(value_list[value_pos1]) * int(value_list[value_pos2])
+            value_list[value_pos3] = str(result)
+
+        else:
+            print('invalid value')
+
+    return value_list
+
+
+if __name__ == '__main__':
+    print(intCode(readValues()))
