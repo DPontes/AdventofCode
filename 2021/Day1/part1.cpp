aa3ba783392a6e079ff6de9c15f23822d5f903c3
DPontes <dpontes@protonmail.com>
2021-12-05 21:29:31 +0100

    Some refactoring

--- 2021/Day1/part1.cpp
+++ 2021/Day1/part1.cpp
@@ -2,37 +2,41 @@
 #include <exception>
 #include <fstream>
 
-int isDeeper(int a, int b)
+const int isDeeper(int* a, int* b)
 {
-  return b > a;
+  return *b > *a;
+}
+
+const int countTotalDepthIncrements(std::ifstream& input)
+{
+  std::string sDepth;
+  int depth;
+  int previous = -1;
+  int totalDepthIncrements = 0;
+
+  while (getline(input, sDepth))
+  {
+    depth = std::stoi(sDepth);
+
+    if(previous == -1) previous = depth;
+    else
+    {
+      if(isDeeper(&previous, &depth)) totalDepthIncrements++;
+      previous = depth;
+    }
+  }
+  return totalDepthIncrements;
 }
 
 int main(int argc, char *argv[])
 {
-  std::string sDepth;
-  int previous = -1;
-  int totalDepthIncrements = 0;
 
   try
   {
     std::ifstream input(argv[1], std::ios::in | std::ios::binary);
     if(!input) throw std::ios::failure("Error opening file!");
 
-      int depth;
-      while (getline(input, sDepth))
-      {
-        depth = std::stoi(sDepth);
-        if(previous == -1)
-        {
-          previous = depth;
-        }
-        else
-        {
-          if(isDeeper(previous, depth)) totalDepthIncrements++;
-          previous = depth;
-        }
-      }
-      std::cout << "Total: " << totalDepthIncrements << std::endl;
+    std::cout << "Total: " << countTotalDepthIncrements( input ) << std::endl;
   }
   catch (const std::exception &e)
   {
