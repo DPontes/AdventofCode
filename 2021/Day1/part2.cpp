7261353c6d7f050a3ec560c5674923c92e6ec2c8
DPontes <dpontes@protonmail.com>
2021-12-06 23:14:47 +0100

    Correct version, working
        Includes README and refactoring

--- 2021/Day1/part2.cpp
+++ 2021/Day1/part2.cpp
@@ -3,9 +3,23 @@
 #include <fstream>
 #include <vector>
 
-const int isDeeper(int* a, int* b)
+const int isHigher(const int* previous, const int* current)
 {
-  return *b > *a;
+  return *previous < *current;
+}
+
+/*
+    Given n, where n is the last element of vector "depths",
+    it returns a pair containing:
+    - sumPrevious: depths[n-1] + depths[n-2] + depths[n-3]
+    - sumCurrent:  depths[n]   + depths[n-1] + depths[n-2]
+*/
+std::pair<int, int> sumsPrevAndDepth(std::vector<int> d)
+{
+  int sumPrevious = ( d.end()[-2] ) + ( d.end()[-3] ) + ( d.end()[-4] );
+  int sumCurrent  = ( d.end()[-1] ) + ( d.end()[-2] ) + ( d.end()[-3] );
+
+  return std::make_pair(sumPrevious, sumCurrent);
 }
 
 const int countTotalDepthIncrements(std::ifstream& input)
@@ -13,8 +27,6 @@ const int countTotalDepthIncrements(std::ifstream& input)
   std::string sDepth;
   std::vector<int> depths;
   int depth;
-  int sumPrev = -1;
-  int sumDepth;
   int totalDepthIncrements = 0;
 
   while (getline(input, sDepth))
@@ -30,11 +42,14 @@ const int countTotalDepthIncrements(std::ifstream& input)
     }
 
     depths.emplace_back(depth);
-    if (depths.size() < 4) continue;
-    sumPrev = (depths.back() - 1) + (depths.back() - 2) + (depths.back() -3 );
-    sumDepth = (depths.back()) + (depths.back() - 1) + (depths.back() - 2);
 
-    if(isDeeper(&sumPrev, &sumDepth)) totalDepthIncrements++;
+    // As the implementation only works when the vector "depths" has 4+ elements,
+    // we skip the rest of the implementation until then
+    if (depths.size() > 3)
+    {
+      const auto [sumPrevious, sumCurrent] = sumsPrevAndDepth(depths);
+      if(isHigher(&sumPrevious, &sumCurrent)) totalDepthIncrements++;
+    }
   }
   return totalDepthIncrements;
 }
--- 2021/README.md
+++ 2021/README.md
@@ -25,3 +25,16 @@
 ```
     if(!input) throw std::ios::failure("Error opening file!");
 ```
+
+### Part 2
+
+- The implementation changed the main function `countTotalDepthIncrements(std::ifstream&)` to include vector `depths` that would be populated with `emplace_back()` for every new element `depth` (as long as this wasn't NaN, otherwise the program would throw an exception and exit).
+  For each of the new elements in `depths` (and the size of `depths` > 3) the function `sumsPrevAndDepth(std::vector<int>) returns a pair of 2 sums:
+
+    - sumPrevious: depths[n-1] + depths[n-2] + depths[n-3]
+    - sumCurrent:  depths[n]   + depths[n-1] + depths[n-2]
+
+    given that `n` is the last element of vector `depths`.
+    These 2 sums are compared (`isHigher(const int*, const int*)` and if `sumCurrent` is bigger than `sumPrevious`, then `totalDepthIncrements` is incremented by 1.
+    The idea to run the operation for every input `depths` is to keep the time complexity of this solution being `O(n)`
+
