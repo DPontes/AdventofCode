f27561935e0688fa11ddb8e4d9e4acee75c9e75f
DPontes <dpontes@protonmail.com>
2021-12-14 00:21:21 +0100

    big chunk of implementation, core dumps.. :(

--- 2021/Day3/part2.cpp
+++ 2021/Day3/part2.cpp
@@ -6,6 +6,9 @@
 #include <exception>
 #include <fstream>
 
+using matrix = std::vector<std::vector<int>>;
+
+
 int convertCharToInt(const char& c)
 {
   return (int) c - 48;
@@ -28,27 +31,129 @@ std::vector<int> appendString2Vector(const std::string& binaryString)
 }
 
 
-void appendVectorToMatrix(std::vector<std::vector<int>>& binaryMatrix, const std::string& binaryString)
+void appendVectorToMatrix(matrix& binaryMatrix, const std::string& binaryString)
 {
   binaryMatrix.emplace_back(appendString2Vector(binaryString));
+}
+
+
+std::vector<int> getCommonVector( const matrix& internalMatrix, const std::string rule)
+{
+  std::vector<int> commonVector;
+  commonVector.resize(internalMatrix[0].size());
+
+  // mostCommonVector populated by the sum of each column
+  // TODO: surely there's a better way...?
+  for(auto it1 = 0; it1 != internalMatrix.size(); it1++)
+  {
+    for(auto it2 = 0; it2 != internalMatrix[it1].size(); it2++)
+    {
+      commonVector[it2] += internalMatrix[it1][it2];
+    }
+  }
+
+  // Transform a vector of sums into a vector of the most common element [0, 1]
+  for(auto it = 0; it != commonVector.size(); it++)
+  {
+    if(rule == "most") commonVector[it] = (commonVector[it] > internalMatrix.size() / 2) ? 1 : 0;
+    else { commonVector[it] = (commonVector[it] > internalMatrix.size() / 2) ? 0 : 1; }
+  }
+
+  return commonVector;
+}
+
+
+std::pair<matrix, matrix> splitMostLessCommonMatrix(matrix& binaryMatrix)
+{
+
+  auto mostCommonVector = getCommonVector(binaryMatrix, "most");
+
+  matrix firstMostCommonMatrix;
+  matrix firstLessCommonMatrix;
+
+  for(auto it = 0; it != binaryMatrix.size(); it++)
+  {
+    if(binaryMatrix[it][0] == 1) firstMostCommonMatrix.emplace_back(binaryMatrix[it]);
+    else firstLessCommonMatrix.emplace_back(binaryMatrix[it]);
+  }
+
+  return std::make_pair(firstMostCommonMatrix, firstLessCommonMatrix);
+}
+
+// Recursive function
+std::vector<int> getReading(matrix internalMatrix, const std::string rule, int index)
+{
+  auto commonVector = getCommonVector(internalMatrix, rule);
+  std::vector<int> rating;
+
+  for(auto it = 0; it != internalMatrix.size(); it++)
+  {
+    if(internalMatrix[it][index] != commonVector[index])
+    {
+      std::cout << "Some issue here... it = " << it << std::endl;
+      internalMatrix.erase(internalMatrix.begin() + it);
+    }
+  }
+
+  index++;
+  if(index < internalMatrix[0].size())
+  {
+    std::vector<int> prev_rating = getReading(internalMatrix, rule, index);
+    rating.insert(rating.begin(), prev_rating.begin(), prev_rating.end());
+  }
+
+  return rating;
+}
+
+
+float getPower(const int& base, const float& exponent)
+{
+  return base * pow(2, exponent);
+}
+
+int binaryVector2Decimal(const std::vector<int> vector)
+{
+  float decimal;
+
+  for(auto it = 0; it != vector.size(); it++)
+  {
+    auto power = vector.size() - it - 1;
+    decimal += getPower(vector[it], power);
+  }
+
+  return (int) decimal;
+}
+
+std::pair<int, int> getRatings(matrix& binaryMatrix)
+{
+  auto [firstMostCommonMatrix, firstLessCommonMatrix] = splitMostLessCommonMatrix(binaryMatrix);
+
+  auto o2reading  = getReading(firstMostCommonMatrix, "most", 1);
+  auto co2reading = getReading(firstLessCommonMatrix, "less", 1);
+
+  o2reading.insert(o2reading.begin(), firstMostCommonMatrix[0][0]);
+  co2reading.insert(co2reading.begin(), firstLessCommonMatrix[0][0]);
+
+  auto o2rating = binaryVector2Decimal(o2reading);
+  auto co2rating = binaryVector2Decimal(co2reading);
+
+  return std::make_pair(o2rating, co2rating);
 }
 
 
 int lifeSupportRating(std::ifstream& input)
 {
   std::string binaryString;
-  size_t nReads = 0;
-  std::vector<std::vector<int>> binaryMatrix;
+  matrix binaryMatrix;
 
   while(getline(input, binaryString))
   {
-    nReads++;
     appendVectorToMatrix(binaryMatrix, binaryString);
   }
 
-  // Continue here, now that the matrix is filled up
+  auto [o2Rating, co2Rating] = getRatings(binaryMatrix);
 
-  return 0;
+  return o2Rating * co2Rating;
 }
 
 
