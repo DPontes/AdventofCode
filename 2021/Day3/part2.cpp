b8788a48fe15ffbcc2ca160e6c6dd3a9dc2aea33
DPontes <dpontes@protonmail.com>
2021-12-15 10:13:04 +0100

    hacky af, needs a lot of refactoring, and a README

--- 2021/Day3/part2.cpp
+++ 2021/Day3/part2.cpp
@@ -55,8 +55,15 @@ std::vector<int> getCommonVector( const matrix& internalMatrix, const std::strin
   // Transform a vector of sums into a vector of the most common element [0, 1]
   for(auto it = 0; it != commonVector.size(); it++)
   {
-    if(rule == "most") commonVector[it] = (commonVector[it] > internalMatrix.size() / 2) ? 1 : 0;
-    else { commonVector[it] = (commonVector[it] > internalMatrix.size() / 2) ? 0 : 1; }
+    float matrixSize = internalMatrix.size();
+    if(rule == "most")
+    {
+        commonVector[it] = (commonVector[it] >= matrixSize / 2.0) ? 1 : 0;
+    }
+    else
+    {
+        commonVector[it] = (commonVector[it] < matrixSize / 2.0) ? 1 : 0;
+    }
   }
 
   return commonVector;
@@ -73,7 +80,7 @@ std::pair<matrix, matrix> splitMostLessCommonMatrix(matrix& binaryMatrix)
 
   for(auto it = 0; it != binaryMatrix.size(); it++)
   {
-    if(binaryMatrix[it][0] == 1) firstMostCommonMatrix.emplace_back(binaryMatrix[it]);
+    if(binaryMatrix[it][0] == mostCommonVector[0]) firstMostCommonMatrix.emplace_back(binaryMatrix[it]);
     else firstLessCommonMatrix.emplace_back(binaryMatrix[it]);
   }
 
@@ -81,25 +88,36 @@ std::pair<matrix, matrix> splitMostLessCommonMatrix(matrix& binaryMatrix)
 }
 
 // Recursive function
-std::vector<int> getReading(matrix internalMatrix, const std::string rule, int index)
+std::vector<int> getReading(const matrix internalMatrix, const std::string rule, int index)
 {
+  matrix newMatrix;
   auto commonVector = getCommonVector(internalMatrix, rule);
   std::vector<int> rating;
 
-  for(auto it = 0; it != internalMatrix.size(); it++)
+  // if there is only one row left, stop and return the rest of the vector
+  if(internalMatrix.size() == 1)
   {
-    if(internalMatrix[it][index] != commonVector[index])
-    {
-      std::cout << "Some issue here... it = " << it << std::endl;
-      internalMatrix.erase(internalMatrix.begin() + it);
-    }
+      std::vector<int> newVectorFromMatrix;
+      for(int i = index; i < internalMatrix[0].size(); i++)
+      {
+        newVectorFromMatrix.emplace_back(internalMatrix[0][i]);
+      }
+      return newVectorFromMatrix;
+  }
+  else
+  {
+      for(auto it = 0; it != internalMatrix.size(); it++)
+      {
+        if(internalMatrix[it][index] == commonVector[index])    newMatrix.emplace_back(internalMatrix[it]);
+      }
+      rating.emplace_back(commonVector[index]);
   }
 
   index++;
-  if(index < internalMatrix[0].size())
+  if(index < newMatrix[0].size())
   {
-    std::vector<int> prev_rating = getReading(internalMatrix, rule, index);
-    rating.insert(rating.begin(), prev_rating.begin(), prev_rating.end());
+    std::vector<int> prev_rating = getReading(newMatrix, rule, index);
+    rating.insert(rating.end(), prev_rating.begin(), prev_rating.end());
   }
 
   return rating;
@@ -111,30 +129,32 @@ float getPower(const int& base, const float& exponent)
   return base * pow(2, exponent);
 }
 
-int binaryVector2Decimal(const std::vector<int> vector)
+
+int binaryVector2Decimal(const std::vector<int>& vector)
 {
   float decimal;
 
   for(auto it = 0; it != vector.size(); it++)
   {
-    auto power = vector.size() - it - 1;
+    int power = vector.size() - it - 1;
     decimal += getPower(vector[it], power);
   }
 
   return (int) decimal;
 }
 
+
 std::pair<int, int> getRatings(matrix& binaryMatrix)
 {
   auto [firstMostCommonMatrix, firstLessCommonMatrix] = splitMostLessCommonMatrix(binaryMatrix);
 
+  // this can be refactored into two calls of a function
   auto o2reading  = getReading(firstMostCommonMatrix, "most", 1);
-  auto co2reading = getReading(firstLessCommonMatrix, "less", 1);
-
   o2reading.insert(o2reading.begin(), firstMostCommonMatrix[0][0]);
-  co2reading.insert(co2reading.begin(), firstLessCommonMatrix[0][0]);
-
   auto o2rating = binaryVector2Decimal(o2reading);
+
+  auto co2reading = getReading(firstLessCommonMatrix, "less", 1);
+  co2reading.insert(co2reading.begin(), firstLessCommonMatrix[0][0]);
   auto co2rating = binaryVector2Decimal(co2reading);
 
   return std::make_pair(o2rating, co2rating);
@@ -164,7 +184,9 @@ int main(int argc, char *argv[])
     std::ifstream input(argv[1], std::ios::in | std::ios::binary);
     if(!input) throw std::ios::failure("Error opening file!");
 
-    std::cout << "Life Support Rating: " << lifeSupportRating( input ) << std::endl;
+    auto result = lifeSupportRating( input);
+
+    std::cout << "Life Support Rating: " << result << std::endl;
   }
   catch (const std::exception &e)
   {
