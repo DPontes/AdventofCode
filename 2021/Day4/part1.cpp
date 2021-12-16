e40d69f499b06376b367496dce4b729dda1b58ab
DPontes <dpontes@protonmail.com>
2021-12-16 21:54:41 +0100

    Initial commit, nothing to see

---
+++ 2021/Day4/part1.cpp
@@ -0,0 +1,22 @@
+#include <fstream>
+#include <iostream>
+#include <exception>
+
+int main(int argc, char *argv[])
+{
+  try
+  {
+    std::ifstream input(argv[1], std::ios::in | std::ios::binary);
+    if(!input) throw std::ios::failure("Error opening file!");
+
+    auto result = getScore( input );
+
+    std::cout << "Score: " << result << std::endl;
+  }
+  catch (const std::exception &e)
+  {
+    std::cerr << e.what() << std::endl;
+  }
+
+  return 0;
+}
